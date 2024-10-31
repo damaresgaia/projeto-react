5c68e9e27b65c3f85500172e28912ca6ccfae51b
damaresgaia <damaresgaia@gmail.com>
2024-10-31 15:27:29 -0300

    Estrutura básica

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,31 @@ function App() {
       <h1>Cadastro de Produtos</h1>
 
       <form>
+        <div className="row">
+          <div className="col">
+            <input
+              type="text"
+              placeholder="Nome do Produto"
+              className="form-control"
+            />
+          </div>
+
+          <div className="col">
+            <input
+              type="text"
+              placeholder="Valor"
+              className="form-control"
+            />
+          </div>
+
+        </div>
+
+        <div className="row">
+
+        </div>
+
+        <button className='btn btn-outline-success'>Inserir</button>
+        <button className='btn btn-outline-primary'>Salvar</button>
 
       </form>
 
