8b5bb648ea9610433cf98a2691da6a2e81691bcb
damaresgaia <damaresgaia@gmail.com>
2024-10-31 21:20:36 -0300

    Formulário

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,10 @@ function App() {
 
   return (
     <div className='container'>
-      <h1>Cadastro de Produtos</h1>
+      <h1 className='mt-3 mb-3'>Cadastro de Produtos</h1>
 
       <form>
-        <div className="row">
+        <div className="row mb-3">
           <div className="col">
             <input
               type="text"
@@ -24,18 +24,54 @@ function App() {
             />
           </div>
 
+          <div className="col">
+            <input
+              type="text"
+              placeholder="Qtd"
+              className="form-control"
+            />
+          </div>
         </div>
 
-        <div className="row">
+        <div className="row mb-3">
+          <div className="col">
+            <input
+              type="text"
+              placeholder="Url da imagem"
+              className="form-control"
+            />
+          </div>
 
         </div>
 
-        <button className='btn btn-outline-success'>Inserir</button>
-        <button className='btn btn-outline-primary'>Salvar</button>
+        <div className='btn-group d-flex gap-3'>
+          <button className='btn btn-outline-success'>Inserir</button>
+          <button className='btn btn-outline-primary'>Salvar</button>
+        </div>
 
       </form>
 
-      <table>
+      <table className='table table'>
+        <thead>
+          <tr>
+            <th scope='col'>#</th>
+            <th scope='col'>Nome do Produto</th>
+            <th scope='col'>Valor</th>
+            <th scope='col'>Qtd</th>
+            <th scope='col'>Url da imagem</th>
+            <th scope='col'>Ações</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th scope='row'>1</th>
+            <td>Nome do Produto</td>
+            <td>Valor</td>
+            <td>Qtd</td>
+            <td>Url da imagem</td>
+            <td>Ações</td>
+          </tr>
+        </tbody>
 
       </table>
 
