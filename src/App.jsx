00614565d46dd1d0ee98c587934649ca4bd7016d
damaresgaia <damaresgaia@gmail.com>
2024-11-03 09:23:24 -0300

    Ajuste na validação do formulário

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,11 @@ import './App.css';
 
 function App() {
 
-  const [id, setId] = useState();
-  const [produto, setProduto] = useState();
-  const [valor, setValor] = useState();
-  const [quantidade, setQuantidade] = useState();
-  const [foto, setFoto] = useState();
+  const [id, setId] = useState('');
+  const [produto, setProduto] = useState('');
+  const [valor, setValor] = useState('');
+  const [quantidade, setQuantidade] = useState('');
+  const [foto, setFoto] = useState('');
 
   const [classInserir, setClassInserir] = useState('');
   const [classAlterar, setClassAlterar] = useState('sumir');
@@ -15,17 +15,17 @@ function App() {
   const Cadastrar = (e) => {
     e.preventDefault()
 
-    if(produto === ""){
+    if (produto === "") {
       alert("Por favor preencha o campo nome do produto")
-    }else if(valor === ""){
+    } else if (valor === "") {
       alert("Por favor preencha o campo valor do produto")
-    }else if(quantidade === ""){
+    } else if (quantidade === "") {
       alert("Por favor preencha o campo quantidade do produto")
-    }else{
+    } else if (foto === "") {
+      alert("Por favor preencha o campo foto do produto")
+    } else {
       alert("Produto cadastrado com sucesso!")
     }
-
-    alert("Cadastrando...")
   }
 
 
@@ -39,16 +39,19 @@ function App() {
             <input
               type="text"
               value={produto}
+              className="form-control"
               placeholder="Nome do Produto"
-              className="form-control"           
+              onChange={(e => setProduto(e.target.value))}
             />
           </div>
 
           <div className="col">
             <input
               type="text"
+              value={valor}
               placeholder="Valor"
               className="form-control"
+              onChange={(e) => setValor(e.target.value)}
             />
           </div>
 
@@ -56,7 +59,9 @@ function App() {
             <input
               type="text"
               placeholder="Qtd"
+              value={quantidade}
               className="form-control"
+              onChange={(e) => setQuantidade(e.target.value)}
             />
           </div>
         </div>
@@ -65,15 +70,16 @@ function App() {
           <div className="col">
             <input
               type="text"
-              placeholder="Url da imagem"
+              value={foto}
               className="form-control"
+              placeholder="Url da imagem"
+              onChange={(e) => setFoto(e.target.value)}
             />
           </div>
-
         </div>
 
         <div className='btn-group d-flex gap-3'>
-          <button className={`btn btn-outline-success ${classInserir}`}>Inserir</button>
+          <button type='submit' className={`btn btn-outline-success ${classInserir}`}>Inserir</button>
           <button className={`btn btn-outline-primary ${classAlterar}`}>Salvar</button>
         </div>
 
